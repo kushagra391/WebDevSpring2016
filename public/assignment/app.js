9bd90cc0cbd1d7489baa21f30500eb6c6980b116
Kushagra Verma <verma.k@husky.neu.edu>
2016-03-08 02:09:16 -0500

    updated app.js, config.js

--- public/assignment/app.js
+++ public/assignment/app.js
@@ -1,3 +1,8 @@
 /**
  * Created by Kushagra on 2/23/2016.
  */
+(function () {
+    "use-strict";
+
+    angular.module("FormBuilderApp", ["ngRoute"]);
+})();
--- public/assignment/config.js
+++ public/assignment/config.js
@@ -1,3 +1,40 @@
 /**
  * Created by Kushagra on 2/23/2016.
  */
+(function () {
+
+    "use-strict";
+    angular
+        .module("FormBuilderApp", ["ngRoute"])
+        .config(function($routeProvider){
+            $routeProvider
+                .when("/home", {
+                    templateUrl: "views/home/home.view.html",
+                    controller: ""
+                })
+                .when("/register", {
+                    templateUrl: "views/users/register.view.html",
+                    controller: ""
+                })
+                .when("/login", {
+                    templateUrl: "views/users/login.view.html",
+                    controller: ""
+                })
+                .when("/profile", {
+                    templateUrl: "views/users/profile.view.html",
+                    controller: ""
+                })
+                .when("/admin", {
+                    templateUrl: "views/admin/admin.view.html",
+                    controller: ""
+                })
+                .when("/form", {
+                    templateUrl: "views/forms/forms.view.html",
+                    controller: ""
+                })
+                .otherwise({
+                    redirectTo: "/home"
+                });
+        });
+
+})();
