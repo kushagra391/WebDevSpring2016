db303e046126f7f8b5d50860ef52e584e595f46b
Kushagra Verma <verma.k@husky.neu.edu>
2016-03-10 02:29:33 -0500

    added feature: login

--- public/assignment/views/users/login.controller.js
+++ public/assignment/views/users/login.controller.js
@@ -10,12 +10,17 @@
         $scope.message = null;
         $scope.error = null;
         $scope.login = function(user) {
+
+            console.log("Login attempted");
+
             var testUser = UserService.findUserByCredentials(user.username, user.password);
             if (testUser != null) {
                 UserService.setCurrentUser(testUser);
                 $location.url('/profile');
+                console.log("Login successful");
             }
             else {
+                console.log("Login failed");
                 $scope.error = "Wrong username / password entered. Please retry."
             }
         };
