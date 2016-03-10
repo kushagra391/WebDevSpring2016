9b954296cd68c7b410760e4912b50eb4ab1391c7
Kushagra Verma <verma.k@husky.neu.edu>
2016-03-10 00:14:11 -0500

    added formService

--- public/assignment/services/forms.service.client.js
+++ public/assignment/services/forms.service.client.js
@@ -1,3 +1,73 @@
-/**
- * Created by Kushagra on 3/9/2016.
- */
+(function () {
+    angular
+        .module('FormBuilderApp')
+        .factory('FormService', FormService);
+
+
+    function FormService() {
+        var model = {
+            forms: [
+                {
+                    "_id": "000",
+                    "title": "Contacts",
+                    "userId": 123
+                },
+                {
+                    "_id": "010",
+                    "title": "TODO",
+                    "userId": 195
+                },
+                {
+                    "_id": "020",
+                    "title": "MP3s",
+                    "userId": 234
+                }
+            ],
+            createFormForUser: createFormForUser,
+            findAllFormsForUser: findAllFormsForUser,
+            deleteFormById: deleteFormById,
+            updateFormById: updateFormById
+        };
+
+        return model;
+    }
+
+    function createFormForUser(userId, form) {
+        var form = {
+            _id: (new Date).getTime(),
+            title: form.title,
+            userId: userId
+        };
+        model.forms.push(form);
+        return form;
+    }
+
+    function findAllFormsForUser(userId) {
+        for (var i in model.forms) {
+            if (model.forms[i].userId === userId) {
+                return model.forms[i];
+            }
+        }
+        return null;
+    }
+
+    function deleteFormById(formId) {
+        for (var i in model.forms) {
+            if (model.forms[i]._id === formId) {
+                model.forms.splice(i, 1);
+            }
+        }
+        return model.forms;
+    }
+
+    function updateFormById(formId, newForm) {
+        for (var i in model.forms) {
+            if (model.forms[i]._id === formId) {
+                model.forms[i].title = newForm.title;
+                model.forms[i].userId = newForm.userId;
+                return model.forms[i];
+            }
+        }
+        return null;
+    }
+})();
