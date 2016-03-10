8b651a42ed4897e47f0cb4429c8d699666c6dd93
Kushagra Verma <verma.k@husky.neu.edu>
2016-03-10 01:26:48 -0500

    added formController

--- public/assignment/views/forms/forms.controller.js
+++ public/assignment/views/forms/forms.controller.js
@@ -1,3 +1,58 @@
-/**
- * Created by Kushagra on 3/7/2016.
- */
+(function () {
+    angular
+        .module("FormBuilderApp")
+        .controller("FormsController", FormsController);
+
+    function FormsController($scope, FormService, UserService) {
+        $scope.error = null;
+        $scope.currentUser = UserService.getCurrentUser();
+        $scope.forms = FormService.findAllForms();
+        $scope.selectedFormId;
+        $scope.addForm = addForm;
+        $scope.updateForm = updateForm;
+        $scope.deleteForm = deleteForm;
+        $scope.selectForm = selectForm;
+
+
+        function addForm(form) {
+            if (!form.title) {
+                $scope.error = "Please provide a form name.";
+                return;
+            }
+            if ($scope.currentUser == null) {
+                $scope.error = "Please login.";
+                return;
+            }
+            var newForm = FormService.createFormForUser($scope.currentUser._id, form);
+            console.log("new from: " + newForm.title);
+        }
+
+        function updateForm(form) {
+            if ($scope.selectedFormId == null) {
+                $scope.error = "Please select a form.";
+                return;
+            }
+            if (form.userId == null) {
+                form.userId = $scope.currentUser._id;
+            }
+            FormService.updateFormById($scope.selectedFormId, form);
+            $scope.selectedFormId = null;
+            form.title = "";
+        }
+
+        function deleteForm(index) {
+            var fromId = $scope.forms[index]._id;
+            FormService.deleteFormById(fromId);
+        }
+
+        function selectForm(index) {
+            $scope.form = {
+                _id: $scope.forms[index]._id,
+                title: $scope.forms[index].title
+            };
+            $scope.selectedFormId = $scope.form._id;
+            console.log("selectedFormId: " + $scope.selectedFormId);
+        }
+    }
+
+})();
--- public/assignment/views/users/login.controller.js
+++ public/assignment/views/users/login.controller.js
@@ -1,3 +1,26 @@
-/**
- * Created by Kushagra on 3/7/2016.
- */
+(function () {
+
+    "use strict";
+
+    angular
+        .module('FormBuilderApp')
+        .controller('LoginController', LoginController);
+
+    function LoginController($scope, $location, UserService) {
+        $scope.message = null;
+        $scope.error = null;
+        $scope.login = function(user) {
+            var testUser = UserService.findUserByCredentials(user.username, user.password);
+            if (testUser != null) {
+                UserService.setCurrentUser(testUser);
+                $location.url('/profile');
+            }
+            else {
+                $scope.error = "Wrong username / password entered. Please retry."
+            }
+        };
+
+    }
+
+
+})();
