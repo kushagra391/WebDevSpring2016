0a4bc3739bc4ed9f34a970efdaa288b9e8568c8e
Kushagra Verma <verma.k@husky.neu.edu>
2016-03-13 00:43:06 -0500

    restarting work: todo-> youtubeAPI

--- public/project/views/home/home.controller.js
+++ public/project/views/home/home.controller.js
@@ -6,6 +6,10 @@
 
     function HomeController($scope, $location) {
         $scope.hello = "Hello from HomeController";
+        $scope.searchKey = "";
+
+        console.log("user entered" + $scope.searchKey);
+        console.log('Inside HomeController');
     }
 
 })();
