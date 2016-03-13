5dfe3c8e4cdd71e3cd581cdac466d2bf5b655e53
Kushagra Verma <verma.k@husky.neu.edu>
2016-03-13 01:43:34 -0500

    setup API call

--- public/project/views/home/home.controller.js
+++ public/project/views/home/home.controller.js
@@ -4,12 +4,24 @@
         .module('CourseraApp')
         .controller('HomeController', HomeController);
 
-    function HomeController($scope, $location) {
+    function HomeController($scope, $http) {
         $scope.searchKey = "";
         $scope.search = search;
 
         function search(keyword) {
             console.log("User pressed: " + keyword);
+
+            var api_key = "AIzaSyDvX6gB6-SABxToMYfQGCFk7SSCvh9bHeg";
+            var url_prefix = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=";
+            var url_suffix = "&key=" + api_key;
+
+            var url = url_prefix + keyword + url_suffix;
+            $http.get(url)
+                .success(render);
+        }
+
+        function render(respose) {
+            console.log(respose);
         }
     }
 
