4f533232c2458ca7175c7fbfb81ffb6d04d4645d
Kushagra Verma <verma.k@husky.neu.edu>
2016-03-19 04:05:53 -0400

    A3: experiments

---
+++ public/experiments/examples/server-side/hello/app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+
+function sayHello(req, res) {
+    res.send('<h2>Say Hello</h2>');
+}
+
+app.get('/api/hello', sayHello);
+app.get('/api/json', function (req, res) {
+
+    var course = [
+        {title: 'Java 101', seats: 12, start: new Date()},
+        {title: 'C# 101', seats: 12, start: new Date()},
+        {title: 'ASP.NET 101', seats: 12, start: new Date()},
+        {title: 'Node.js 101', seats: 12, start: new Date()},
+        {title: 'AngularJS 101', seats: 12, start: new Date()},
+    ];
+    res.send(course);
+
+});
+
+
+app.listen(3000);
+console.log("Server running@: http://localhost:3000/");
