7d19127817833ee241a88ae81d2a40f40c6f5495
Kushagra Verma <verma.k@husky.neu.edu>
2016-03-19 04:49:03 -0400

    A3: added more experiments

---
+++ public/experiments/examples/server-side/renderJSON/app.js
@@ -0,0 +1,29 @@
+(function () {
+
+    "use strict";
+
+    angular
+        .module('App')
+        .controller('MainController', MainController);
+
+    function MainController($scope, $http) {
+        // $scope.courses = [
+        //     {title: 'Java 101', seats: 12, start: new Date()},
+        //     {title: 'C# 101', seats: 12, start: new Date()},
+        //     {title: 'ASP.NET 101', seats: 12, start: new Date()},
+        //     {title: 'Node.js 101', seats: 12, start: new Date()},
+        //     {title: 'AngularJS 101', seats: 12, start: new Date()}
+        // ];
+        //
+        // console.log($scope.courses);
+
+        $http.get("/api/json")
+            .success(function(response){
+                console.log("ajax call succesfull");
+                $scope.courses = response;
+            });
+
+        console.log($scope.courses);
+    }
+
+})();
