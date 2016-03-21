73e09712325e7cc17a8a80a12f6e1e0cc0396e2f
Kushagra Verma <verma.k@husky.neu.edu>
2016-03-21 07:47:44 -0400

    A3: implemented server/form.model.js

--- public/assignment/server/models/form.model.js
+++ public/assignment/server/models/form.model.js
@@ -19,34 +19,74 @@ module.exports = function (app) {
     return api;
 
     function createFormForUser(userId, form) {
+        form._id = uuid.v4();
+        form.userId = userId;
+        mockForms.push(form);
 
+        return mockForms;
     }
 
     function createFieldInForm(formId, field) {
-
+        var form = findFormById(formId);
+        field._id = uuid.v4();
+        form.fields.push(field);
+        return form;
     }
 
     function findAllForms() {
-
+        return mockForms;
     }
 
     function findFormById(formId) {
+        for (var index in mockForms) {
+            if (mockForms[index]._id === formId) {
+                return mockForms[index];
+            }
+        }
 
+        return null; // if not found
     }
 
     function updateForm(formId, newForm) {
+        for (var index in mockForms) {
+            if (formId === mockForms[index]._id) {
+                mockForms[index].title = newForm.title;
+                mockForms[index].userId = newForm.userId;
+                mockForms[index].fields = newForm.fields;
+            }
+        }
 
+        return mockForms;
     }
 
     function deleteForm(formId) {
 
+        for (var index in mockForms) {
+            if (mockForms[index]._id === formId) {
+                mockForms.splice(index, 1);
+            }
+        }
+
+        return mockForms;
     }
 
     function findFormByTitle(title) {
+        for (var index in mockForms) {
+            if (mockForms[index].title === title) {
+                return mockForms[index];
+            }
+        }
 
+        return null;
     }
 
     function findFormByUser(userId) {
+        for (var index in mockForms) {
+            if (mockForms[index].userId === userId) {
+                return mockForms[index];
+            }
+        }
 
+        return null;
     }
 }
