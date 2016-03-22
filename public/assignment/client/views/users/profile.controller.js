d7060d9bab3f24cdf7802ce7ac50816d9544318a
Kushagra Verma <verma.k@husky.neu.edu>
2016-03-22 03:07:22 -0400

    A3: updated profileController

--- public/assignment/client/views/users/profile.controller.js
+++ public/assignment/client/views/users/profile.controller.js
@@ -10,25 +10,32 @@
         $scope.currentUser = UserService.getCurrentUser();
         $scope.errorMessage = null;
         $scope.message = null;
+
         $scope.id = $routeParams.id;
+
+        $scope.update = update;
 
         // TODO: when or how does the controller method get executed
         if (!$scope.currentUser) {
             $location.url("/home");
         }
 
-        $scope.update = function (user) {
-            UserService.updateUser($routeParams.id, user);
-
-            if (user) {
-                $scope.message = "Successful: user updated.";
-                UserService.setCurrentUser($scope.currentUser);
+        function update(user) {
+            if (!user) {
+                $scope.errorMessage = 'Error: User not populated correctly';
             } else {
-                $scope.errorMessage = "Failed: User not updated.";
-                console.log("Failed: user not updated")
+                console.log('INFO: User getting updated');
+                UserService
+                    .updateUser($routeParams.id, user)
+                    .then (function (response) {
+                        if (response.data) {
+                            UserService.setCurrentUser($scope.currentUser);
+                            $scope.message = 'Success: User updated succesfully';
+                        } else {
+                            $scope.errorMessage = 'Failure: User Update Failed';
+                        }
+                    })
             }
-
-            console.log("List of All Users : " + UserService.findAllUsers());
-        };
+        }
     }
 })();
