72dd48342db5b8936b1e6fc5dc48fc2b544686ed
Kushagra Verma <verma.k@husky.neu.edu>
2016-03-23 08:19:17 -0400

    openshift issue

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var bodyParser = require('body-parser');
 var multer = require('multer');
 var db;
 
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname + '/public'));
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
