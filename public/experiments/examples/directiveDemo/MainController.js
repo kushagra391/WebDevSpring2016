5776eb870e3825f7a4f768b7f53c7e0755b6b515
Kushagra Verma <verma.k@husky.neu.edu>
2016-03-30 20:38:32 -0400

    sortable jgaTable

--- public/experiments/examples/directiveDemo/MainController.js
+++ public/experiments/examples/directiveDemo/MainController.js
@@ -7,28 +7,58 @@
         .controller('MainController', MainController);
 
     function MainController($scope) {
-        $scope.users = [
-            {
-                "name" : "Kushagra",
-                "age" : 27,
-                "gender" : "Male"
-            },
-            {
-                "name" : "Amrit",
-                "age" : 28,
-                "gender" : "Male"
-            },
-            {
-                "name" : "Bhatta",
-                "age" : 27,
-                "gender" : "Male"
-            },
-            {
-                "name" : "Shruti",
-                "age" : 27,
-                "gender" : "Female"
-            }
-        ];
+
+        $scope.users = getUsers();
+        function getUsers() {
+            var users = [
+                {
+                    "name" : "Kushagra",
+                    "age" : 27,
+                    "gender" : "Male"
+                },
+                {
+                    "name" : "Amrit",
+                    "age" : 28,
+                    "gender" : "Male"
+                },
+                {
+                    "name" : "Bhatta",
+                    "age" : 27,
+                    "gender" : "Male"
+                },
+                {
+                    "name" : "Shruti",
+                    "age" : 27,
+                    "gender" : "Female"
+                }
+            ];
+
+            return users;
+        }
+        
+
+        // $scope.users = [
+        //     {
+        //         "name" : "Kushagra",
+        //         "age" : 27,
+        //         "gender" : "Male"
+        //     },
+        //     {
+        //         "name" : "Amrit",
+        //         "age" : 28,
+        //         "gender" : "Male"
+        //     },
+        //     {
+        //         "name" : "Bhatta",
+        //         "age" : 27,
+        //         "gender" : "Male"
+        //     },
+        //     {
+        //         "name" : "Shruti",
+        //         "age" : 27,
+        //         "gender" : "Female"
+        //     }
+        // ];
     }
 
 })();
--- public/experiments/examples/directiveDemo/directive/jgaTable.js
+++ public/experiments/examples/directiveDemo/directive/jgaTable.js
@@ -12,16 +12,23 @@
 
         function link(scope, element, attributes) {
             var jgaAxis = attributes.jgaAxis;
+
             $(element).sortable({
                 axis: jgaAxis,
                 start: function (event, ui) {
                     start = ui.item.index();
+                    console.log('start: ' + start);
                 },
                 stop: function (event, ui) {
                     end = ui.item.index();
+                    console.log('end: ' + end);
+
+                    // swap entries in scope [edit scope]
                     var temp = scope.users[start];
                     scope.users[start] = scope.users[end];
                     scope.users[end] = temp;
+
+                    // apply the changes to scope [set scope]
                     scope.$apply();
                 }
             });
