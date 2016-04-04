015219a15561e41f86bca66e7bdd62acb1a9f0b8
Kushagra Verma <verma.k@husky.neu.edu>
2016-04-04 17:58:33 -0400

    loaded UserModel from UserSchema

--- public/experiments/examples/mongoDemo/server/models/user.model.server.js
+++ public/experiments/examples/mongoDemo/server/models/user.model.server.js
@@ -5,8 +5,14 @@ module.exports = function (db, mongoose) {
     "use strict";
 
     var mockForm = require('./user.model.mock.json');
+
+    // load user schema
     var UserSchema = require('./user.schema.server')(mongoose);
 
+    // create user model from schema
+    var UserModel = mongoose.model('User', UserSchema);
+
+    
     var api = {
         findAllUsers : findAllUsers,
         addUser : addUser
