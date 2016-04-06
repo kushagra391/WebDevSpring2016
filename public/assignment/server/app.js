7b878d2d6a7a788fe7b1d48a48e5bd9eb954a689
Kushagra Verma <verma.k@husky.neu.edu>
2016-04-06 15:28:58 -0400

    a4: refactored app.js to include fieldModel

--- public/assignment/server/app.js
+++ public/assignment/server/app.js
@@ -3,8 +3,9 @@ module.exports = function (app, db, mongoose) {
     console.log('Starting A3 Server...');
     var userModel = require('./models/user.model.js')(db, mongoose);
     var formModel = require('./models/form.model.js')(db, mongoose);
-
+    var fieldModel = require('./models/field.model.server')(db, mongoose);
+    
     var userService = require('./services/user.service.server.js')(app, userModel);
-    var formService = require('./services/form.service.js')(app, formModel);
+    var formService = require('./services/form.service.js')(app, formModel, fieldModel);
     var fieldService = require('./services/field.service.server.js')(app, formModel);
 };
