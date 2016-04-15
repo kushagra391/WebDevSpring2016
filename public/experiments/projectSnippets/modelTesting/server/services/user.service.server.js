eedbdce08db59ad8f8c91c434186d62445549f47
Kushagra Verma <verma.k@husky.neu.edu>
2016-04-15 02:37:47 -0400

    snippet*: + req.params.xx

--- public/experiments/projectSnippets/modelTesting/server/services/user.service.server.js
+++ public/experiments/projectSnippets/modelTesting/server/services/user.service.server.js
@@ -2,8 +2,8 @@ module.exports = function (app, userModel) {
 
     "use strict";
 
-    app.get("/api/test/user/:username", findUserByUsername);
-    app.get("/api/test/user/:name", findUserByName);
+    app.get("/api/test/user/username/:username", findUserByUsername);
+    app.get("/api/test/user/name/:name", findUserByName);
     app.get("/api/test/user/all", findAllUsers);
 
     app.post("/api/test/user/login", findUserByCredentials);
@@ -16,36 +16,61 @@ module.exports = function (app, userModel) {
     app.delete("/api/test/user/:name", deleteUserByName);
 
 
-    function findUserByUsername() {
+    function findUserByUsername(req, res) {
+        console.log(">> findUserByUsername");
+        var username = req.params.username;
+        console.log(username);
+
+
+        res.json(200);
 
     }
 
-    function findUserByName() {
+    function findUserByName(req, res) {
+        console.log(">> findUserByName");
 
+        res.json(200);
     }
 
-    function findAllUsers() {
+    function findAllUsers(req, res) {
+        console.log(">> findAllUsers");
 
+        res.json(200);
     }
 
-    function findUserByCredentials() {
+    function findUserByCredentials(req, res) {
+        console.log(">> findUserByCredentials");
 
+        res.json(200);
     }
 
-    function createUser() {
+    function createUser(req, res) {
+        console.log(">> createUser");
 
+        res.json(200);
     }
 
-    function updateUserById() {
+    function updateUserById(req, res) {
+        console.log(">> updateUserById");
 
+        res.json(200);
     }
 
-    function deleteUserById() {
+    function updateUserByName(req, res) {
+        console.log(">> updateUserByName");
 
+        res.json(200);
     }
 
-    function deleteUserByName() {
+    function deleteUserById(req, res) {
+        console.log(">> deleteUserById");
+        res.json(200);
+    }
 
+    function deleteUserByName(req, res) {
+        console.log(">> deleteUserByName");
+
+        res.json(200);
     }
 
 };
